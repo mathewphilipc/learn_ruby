2bc85b272edb51cfa8a64ac4f96acff504953b4b
Mathew Calkins <mathewpcalkins@gmail.com>
2017-03-22 20:49:11 -0400

    Added functionality to temperature converter

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+#some auxiliary quantities for legibility
+def ftoc(fTemp)
+  cTemp = (fTemp - 32.0)*5.0/9.0
+  return cTemp
+end
+
+def ctof(cTemp)
+  fTemp = (cTemp)*9.0/5.0 + 32.0
+  return fTemp
+end
+
