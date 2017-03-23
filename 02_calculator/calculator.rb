6e713c8ddc5dfcc3a0f8beba2501d34ae3dcff66
Mathew Calkins <mathewpcalkins@gmail.com>
2017-03-22 21:15:47 -0400

    Added functionality to basic calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add(a, b)
+  return a+b
+end
+
+def subtract(a,b)
+  return a-b
+end
+
+def sum(arrayIn)
+  output = 0
+  if (arrayIn.length > 0)
+    arrayIn.each {
+      |x| output = output + x
+    }
+  end
+  return output
+end
